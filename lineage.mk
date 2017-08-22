56def0d30feb7a1333a3ada76c5f8bb2153b69f5
Aaron Kling <webgeek1234@gmail.com>
2017-08-22 15:07:35 -0500

    CM vendor renamed to lineage

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_shieldtablet
 PRODUCT_DEVICE := shieldtablet
